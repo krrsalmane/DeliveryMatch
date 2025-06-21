d685ac53d5c2e863ab8cdeae45f483c0c9539a72
krrsalmane <karrounsalmane@gmail.com>
2025-06-22 00:08:19 +0100

    register

--- frontendD/src/app/app.ts
+++ frontendD/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Createannonce } from './components/createannonce/createannonce';
+import { Registerprofile } from './components/registerprofile/registerprofile';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Createannonce],
+  imports: [Createannonce,Registerprofile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
